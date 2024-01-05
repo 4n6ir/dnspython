eba333944d5c903ae2833efd17eb1a7479a93fe2
John Lukach <hello@lukach.io>
2024-01-05 00:04:03 +0000

    dnspython 2.4.2

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dnspython
+
+##### Cloud Development Kit (CDK) v2
+
+```python
+        region = Stack.of(self).region
+
+        dnspython = _lambda.LayerVersion.from_layer_version_arn(
+            self, 'dnspython',
+            layer_version_arn = 'arn:aws:lambda:'+region+':070176467818:layer:dnspython:1'
+        )
+```
+
+##### AWS Lambda Layer for Python Library
+
+ 1. ```cd bundle```
+ 2. ```mkdir python```
+ 3. ```cd python```
+ 4. ```pip3 install dnspython -t .```
+ 5. ```cd ..```
+ 6. ```zip -r dnspython.zip .```
+ 7. ```rm -R python```
---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from dnspython.dnspython_stack import DnspythonStack
+
+app = cdk.App()
+
+DnspythonStack(
+    app, 'DnspythonStackUSE1',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DnspythonStack(
+    app, 'DnspythonStackUSE2',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DnspythonStack(
+    app, 'DnspythonStackUSW2',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-west-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+cdk.Tags.of(app).add('Alias','Extensions')
+cdk.Tags.of(app).add('GitHub','https://github.com/4n6ir/dnspython.git')
+
+app.synth()
